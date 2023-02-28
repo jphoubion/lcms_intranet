cf5a22fc1875f0e27b13feeb16adc609a959b230
jphoubion <jp.houbion@gmail.com>
2023-02-28 22:37:29 +0100

    * création des bases de modèle pour GesDoc : Documents, DocumentsCategories

--- gesdoc/admin.py
+++ gesdoc/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DocumentCategories, Documents
 
-# Register your models here.
+
+admin.site.register(Documents)
+admin.site.register(DocumentCategories)
--- gesdoc/models.py
+++ gesdoc/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class DocumentCategories(models.Model):
+
+    name = models.CharField(verbose_name="Nom", max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Document categories'
+
+    def __str__(self):
+        return self.name
+class Documents(models.Model):
+
+    name = models.CharField(verbose_name="Nom", max_length=255)
+    description = models.TextField(verbose_name="Description")
+    category = models.ForeignKey(DocumentCategories, verbose_name="Catégorie", related_name="category",
+                                 on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Documents'
+
+    def __str__(self):
+        return self.name
